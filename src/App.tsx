1bdc2b168f446984d6597302d27ec49d49dd2426
Jaspreet Singh Nahal <jaspreetnahal100@gmail.com>
2025-03-26 16:42:01 +0530

    j

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "./components/ui/toaster";
 import NotFound from "./pages/not-found";
 import AuthPage from "./pages/auth-page";
-import { AuthProvider } from "./hooks/use-auth";
+import { AuthProvider } from "./hooks/use-auth.tsx";
 import { ProtectedRoute } from "./lib/protected-route";
 import DashboardPage from "./pages/dashboard-page";
 import PerformancePage from "./pages/performance-page.tsx";
