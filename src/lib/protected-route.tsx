e7cfffaf8d41c7932b112bab19325c1f8f2bf24a
Jaspreet Singh Nahal <jaspreetnahal100@gmail.com>
2025-03-26 17:03:27 +0530

    g

--- src/lib/protected-route.tsx
+++ src/lib/protected-route.tsx
@@ -1,6 +1,7 @@
-import { useAuth } from "@/hooks/use-auth";
+import { useAuth } from "../hooks/use-auth";
 import { Loader2 } from "lucide-react";
 import { Redirect, Route } from "wouter";
+
 
 export function ProtectedRoute({
   path,
