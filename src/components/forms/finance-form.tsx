97f1038cc0210329cdfa1578e32fd8aa3e2eea18
Jaspreet Singh Nahal <jaspreetnahal100@gmail.com>
2025-03-26 17:50:23 +0530

    g

--- src/components/forms/finance-form.tsx
+++ src/components/forms/finance-form.tsx
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form";
-import { zodResolver } from "../hookform/resolvers/zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { insertFinanceSchema } from "../../components/schema";
 import { Button } from "../../components/ui/button";
